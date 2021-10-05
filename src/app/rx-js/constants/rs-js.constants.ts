623e8d4dec0581d73344bee503972d53c45ea143
Gena3272 <genabirchenko@icloud.com>
2021-10-05 13:18:42 +0300

    * Refactoring

--- src/app/rx-js/constants/rs-js.constants.ts
+++ src/app/rx-js/constants/rs-js.constants.ts
@@ -1,2 +1,2 @@
 export const B = 'b';
-export const EXAMPLERESULT = [1, 2, 3, 4, 8];
+export const EXAMPLE_RESULT = [1, 2, 3, 4, 8];
--- src/app/rx-js/rx-js.component.ts
+++ src/app/rx-js/rx-js.component.ts
@@ -6,7 +6,7 @@ import { interval, of, Subject } from "rxjs";
 import { filter, map, takeUntil, takeWhile, tap } from "rxjs/operators";
 
 // Constants
-import { B, EXAMPLERESULT } from "./constants/rs-js.constants";
+import { B, EXAMPLE_RESULT } from "./constants/rs-js.constants";
 
 @Component({
   selector: 'app-rx-js',
@@ -20,36 +20,6 @@ export class RxJsComponent implements OnInit, OnDestroy {
   private resultValidityList: number[] = [];
   private unsubscribe$ = new Subject<void>();
 
-  filterOfNums(): void {
-    this.filteringNumbers$
-      .pipe(
-        filter((v) => v === 1),
-        takeUntil(this.unsubscribe$),
-      )
-      .subscribe();
-  }
-
-  modificationObj(): void {
-    this.usersData$
-      .pipe(
-        filter((v) => v.name === B),
-        map((v) => [{...v, age: 22 }]),
-        takeUntil(this.unsubscribe$),
-      )
-      .subscribe();
-  }
-
-  actionsDataInInterval(): void {
-    this.interval$
-      .pipe(
-        takeWhile(() => EXAMPLERESULT.toString() !== this.resultValidityList.toString()),
-        filter((v) => EXAMPLERESULT.includes(v)),
-        tap(v => this.resultValidityList.push(v)),
-        takeUntil(this.unsubscribe$),
-      )
-      .subscribe();
-  }
-
   ngOnInit(): void {
     this.filterOfNums();
     this.modificationObj();
@@ -59,5 +29,35 @@ export class RxJsComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
+  }
+
+  private filterOfNums(): void {
+    this.filteringNumbers$
+      .pipe(
+        filter((v) => v === 1),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe();
+  }
+
+  private modificationObj(): void {
+    this.usersData$
+      .pipe(
+        filter((v) => v.name === B),
+        map((v) => [{...v, age: 22 }]),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe();
+  }
+
+  private actionsDataInInterval(): void {
+    this.interval$
+      .pipe(
+        takeWhile(() => EXAMPLE_RESULT.toString() !== this.resultValidityList.toString()),
+        filter((v) => EXAMPLE_RESULT.includes(v)),
+        tap(v => this.resultValidityList.push(v)),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe();
   }
 }
